176de68bb16f8b8d54c6c9c60c2e8744e17ad290
Romango <homer37@yandex.ru>
2022-09-23 08:23:15 +0400

    first one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactPlayer from "react-player/youtube";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { setDefaultNamespace } from "i18next";
+
+const broadcats = [
+  {
+    id: 1,
+    title: "Russia",
+    language: "ru",
+    link: "https://www.youtube.com/embed/ysz5S6PUM-U?rel=0?autoplay=1",
+  },
+  {
+    id: 2,
+    title: "England",
+    language: "en",
+    link: "https://www.youtube.com/embed/ysz5S6PUM-U?rel=0?autoplay=1",
+  },
+  {
+    id: 3,
+    title: "Deutch",
+    language: "de",
+    link: "https://www.youtube.com/embed/ysz5S6PUM-U?rel=0?autoplay=1",
+  },
+  {
+    id: 5,
+    title: "Spain",
+    language: "sp",
+    link: "https://www.youtube.com/embed/ysz5S6PUM-U?rel=0?autoplay=1",
+  },
+];
 
 function App() {
+  const [activeVideo, setActiveVideo] = useState({});
+
+  const defaultVideo = {
+    id: 5,
+    title: "Spain",
+    language: "sp",
+    link: "https://www.youtube.com/embed/FOxf1iBFgdM?autoplay=1",
+  };
+
+  const getVideo = (arr) => {
+    const currenLanguage = window.navigator.language;
+    const video = arr.filter((item) => item.language === currenLanguage);
+    if (video.lenth) {
+      return video[0];
+    } else {
+      return arr[0];
+    }
+  };
+
+  const getData = async () => {
+    const options = {
+      method: "GET",
+    };
+    const data= await fetch("https://api1211.herokuapp.com/players", options)
+      .then((response) => response.json())
+
+    setActiveVideo(getVideo(data.broadcasts));
+  };
+
+  useEffect(() => {
+    getData();
+    setTimeout(() => {
+      setActiveVideo(defaultVideo);
+    }, 15000);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Привет друзья!</h1>
+      <div className="player-wrapper">
+        <ReactPlayer
+          autoplay
+          className="react-player"
+          playing={true}
+          controls
+          url={activeVideo.link}
+        />
+      </div>
+      <iframe
+        width="560"
+        height="315"
+        src={activeVideo.link}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
